f58c0de31771f09038b65a706776bf69f7325ad7
CodeMehringer <153135304+CodeMehringer@users.noreply.github.com>
2024-03-26 19:03:47 -0500

    resolved start button not displaying and gamebord not showing when start pushed

--- git-game/src/App.js
+++ git-game/src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import GameSection from './GameSection';
-import './Game.css'
+import './Game.css'; 
 
 function App() {
   return (
     <div className='App'>
-      {/* Bootstrap CSS */}
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
       
-      {/* GameSection component */}
       <GameSection />
-      
-      {/* Bootstrap JavaScript */}
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-      {/* jQuery */}
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous"></script>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-      {/* Your custom JavaScript */}
-      <script src="./Game1.js"></script>
+ 
     </div>
   );
 }
--- git-game/src/GameSection.js
+++ git-game/src/GameSection.js
@@ -1,283 +1,39 @@
-import React from 'react';
-import './Game.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import './Game.css'; 
 
 function GameSection() {
   const [startGameVisible, setStartGameVisible] = useState(false);
 
-  const startGame =() => {
-
+  const startGame = () => {
     setStartGameVisible(true);
   }
+
   return (
     <div id="gameSection">
       <button onClick={startGame}>Start Game</button>
-        <div className="flex-container"> </div>
-        <div class="row row1">
-          <div class="gameSection animated rotateIn"></div>
-          <div id="startGame" className={startGameVisible ? 'active' : ''}></div>
-          <div class="flex-box" id="cell1">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell2">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell3">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell4">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div> 
-          </div>
-          <div class="flex-box" id="cell5">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell6">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>  
-          </div>
-          <div class="flex-box" id="cell8">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell9">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div> 
-          <div class="flex-box" id="cell10">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-        <div class="column">
-          <div class="row row3">
-          <div class="flex-box" id="cell30">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell29">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell28">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell27">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell26">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell25">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell24">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell23">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell22">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell21">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-        </div>         
+      {startGameVisible && ( 
+        <div className="gameSection active">
+        {/* Game board */}
+        <div className="flex-container">
+          {[...Array(10)].map((_, index) => (
+            <div className="flex-box" key={`row${index}`}>
+              {/* Cells */}
+              {[...Array(10)].map((_, cellIndex) => (
+                <div className="cell" key={`cell${index}-${cellIndex}`}>
+                  {/* Pieces */}
+              <div className="piece" id={`redPiece${index}-${cellIndex}`}></div>
+              <div className="piece" id={`bluePiece${index}-${cellIndex}`}></div>
+              <div className="piece" id={`greenPiece${index}-${cellIndex}`}></div>
+              <div className="piece" id={`yellowPiece${index}-${cellIndex}`}></div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+      )}
     </div>
-    <div class="circle">
-      <div class="flex-circle small" id='circleOne'>
-        <div class="piece" id="redPiece"></div>
-      </div>
-      <div class="flex-circle big" id='circleDice'>
-        <div class="dice"></div>
-      </div> 
-      <div class="flex-circle small" id='circleTwo'>
-        <div class="piece" id="bluePiece"></div>
-      </div>
-      <div class="flex-circle small" id='circleThree'>
-        <div class="piece" id="greenPiece"></div> 
-      </div>
-      <div class="flex-circle small" id='circleFour'>
-        <div class="piece" id="yellowPiece"></div> 
-      </div>
-    </div>
- 
-    <div class="row row2">
-      <div class="flex-box" id="cell11">
-      <div class="piece" id="redPiece"></div>
-      <div class="piece" id="bluePiece"></div>
-      <div class="piece" id="greenPiece"></div>
-      <div class="piece" id="yellowPiece"></div>
-    </div>
-        <div class="flex-box" id="cell12">
-        <div class="piece" id="redPiece"></div>
-        <div class="piece" id="bluePiece"></div>
-        <div class="piece" id="greenPiece"></div>
-        <div class="piece" id="yellowPiece"></div>
-      </div>
-      <div class="flex-box" id="cell13">
-        <div class="piece" id="redPiece"></div>
-        <div class="piece" id="bluePiece"></div>
-        <div class="piece" id="greenPiece"></div>
-        <div class="piece" id="yellowPiece"></div>
-        </div>
-        <div class="flex-box" id="cell14">
-          <div class="piece" id="redPiece"></div>
-          <div class="piece" id="bluePiece"></div>
-          <div class="piece" id="greenPiece"></div>
-          <div class="piece" id="yellowPiece"></div>
-        </div>
-        <div class="flex-box" id="cell15">
-          <div class="piece" id="redPiece"></div>
-          <div class="piece" id="bluePiece"></div>
-          <div class="piece" id="greenPiece"></div>
-          <div class="piece" id="yellowPiece"></div>
-        </div>
-        <div class="flex-box" id="cell16">
-          <div class="piece" id="redPiece"></div>
-          <div class="piece" id="bluePiece"></div>
-          <div class="piece" id="greenPiece"></div>
-          <div class="piece" id="yellowPiece"></div>
-        </div>
-        <div class="flex-box" id="cell17">
-          <div class="piece" id="redPiece"></div>
-          <div class="piece" id="bluePiece"></div>
-          <div class="piece" id="greenPiece"></div>
-          <div class="piece" id="yellowPiece"></div>
-        </div>
-        <div class="flex-box" id="cell18">
-          <div class="piece" id="redPiece"></div>
-          <div class="piece" id="bluePiece"></div>
-          <div class="piece" id="greenPiece"></div>
-          <div class="piece" id="yellowPiece"></div>
-        </div>
-        <div class="flex-box" id="cell19">
-          <div class="piece" id="redPiece"></div>
-          <div class="piece" id="bluePiece"></div>
-          <div class="piece" id="greenPiece"></div>
-          <div class="piece" id="yellowPiece"></div>
-        </div>
-        <div class="flex-box" id="cell30">
-          <div class="piece" id="redPiece"></div>
-          <div class="piece" id="bluePiece"></div>
-          <div class="piece" id="greenPiece"></div>
-          <div class="piece" id="yellowPiece"></div>
-        </div>
-        <div class="row row2">
-          <div class="flex-box" id="cell31">
-          <div class="piece" id="redPiece"></div>
-          <div class="piece" id="bluePiece"></div>
-          <div class="piece" id="greenPiece"></div>
-          <div class="piece" id="yellowPiece"></div>
-        </div>
-            <div class="flex-box" id="cell32">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-          </div>
-          <div class="flex-box" id="cell33">
-            <div class="piece" id="redPiece"></div>
-            <div class="piece" id="bluePiece"></div>
-            <div class="piece" id="greenPiece"></div>
-            <div class="piece" id="yellowPiece"></div>
-            </div>
-            <div class="flex-box" id="cell34">
-              <div class="piece" id="redPiece"></div>
-              <div class="piece" id="bluePiece"></div>
-              <div class="piece" id="greenPiece"></div>
-              <div class="piece" id="yellowPiece"></div>
-            </div>
-            <div class="flex-box" id="cell35">
-              <div class="piece" id="redPiece"></div>
-              <div class="piece" id="bluePiece"></div>
-              <div class="piece" id="greenPiece"></div>
-              <div class="piece" id="yellowPiece"></div>
-            </div>
-            <div class="flex-box" id="cell36">
-              <div class="piece" id="redPiece"></div>
-              <div class="piece" id="bluePiece"></div>
-              <div class="piece" id="greenPiece"></div>
-              <div class="piece" id="yellowPiece"></div>
-            </div>
-            <div class="flex-box" id="cell37">
-              <div class="piece" id="redPiece"></div>
-              <div class="piece" id="bluePiece"></div>
-              <div class="piece" id="greenPiece"></div>
-              <div class="piece" id="yellowPiece"></div>
-            </div>
-            <div class="flex-box" id="cell38">
-              <div class="piece" id="redPiece"></div>
-              <div class="piece" id="bluePiece"></div>
-              <div class="piece" id="greenPiece"></div>
-              <div class="piece" id="yellowPiece"></div>
-            </div>
-            <div class="flex-box" id="cell39">
-              <div class="piece" id="redPiece"></div>
-              <div class="piece" id="bluePiece"></div>
-              <div class="piece" id="greenPiece"></div>
-              <div class="piece" id="yellowPiece"></div>
-            </div>
-            <div class="flex-box" id="cell40">
-              <div class="piece" id="redPiece"></div>
-              <div class="piece" id="bluePiece"></div>
-              <div class="piece" id="greenPiece"></div>
-              <div class="piece" id="yellowPiece"></div>
-            </div>
-        </div>      
-      </div>
-    </div>
-  </div>
-  );     
+  );
 }
+
 export default GameSection;
